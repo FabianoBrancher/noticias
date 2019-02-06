630d49ac2ef8497dd49e3ed088e5387e801b8274
fabianobrancher <fabiano.brancher@hotmail.com>
2019-02-06 20:20:06 -0200

    createInitialUser

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path')
 const port = process.env.port || 3000
 const mongoose = require('mongoose')
 
+const User = require('./models/user')
+
 mongoose.Promise = global.Promise
 
 const mongo = process.env.MONGODB || 'mongodb://localhost/noticias'
@@ -13,6 +15,20 @@ app.set('view engine', 'ejs')
 
 app.use(express.static('public'))
 
+const createInitialUser = async () => {
+    const total = await User.countDocuments({username: 'fabianobrancher'})
+    if (total === 0) {
+        const user = new User({
+            username: 'fabianobrancher',
+            password: 'abc123'
+        })
+        await user.save()
+        console.log('User created.')
+    } else {
+        console.log('User created skipped.')
+    }
+}
+
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -20,7 +36,20 @@ app.get('/', (req, res) => {
 mongoose
     .connect(mongo)
     .then(() => {
+        createInitialUser()
         app.listen(port, () => console.log('Listening....'))
     })
     .catch((error) => console.log(error))
 
+
+
+
+/*
+const User = require('./models/user')
+const user = new User({
+    username: 'fabianobrancher',
+    password: 'abc123'
+})
+
+user.save( () => console.log('opa'))
+*/
